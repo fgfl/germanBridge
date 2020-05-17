21ed33b39c3dc8d3ab9637276aa63402431472ed
fgfl <frederick.gfl@gmail.com>
2020-05-17 11:56:38 -0700

    Add styling to form

--- src/components/UsernameForm/UsernameForm.jsx
+++ src/components/UsernameForm/UsernameForm.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import './UsernameForm.scss';
+
 export default function UsernameForm() {
   return (
     <form onSubmit={(e) => e.preventDefault()}>
       <h2>Enter a username</h2>
-      <label for="id-username">Username:</label>
-      <input type="text" id="id-username"></input>
+      <label htmlFor="usernameField">Username:</label>
+      <input type="text" name="usernameField" id="usernameField"></input>
       <button onClick={(e) => e.preventDefault()}>Save</button>
     </form>
   );
