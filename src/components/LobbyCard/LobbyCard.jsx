118f97acfc38488da0ee2e9e5d1c8d290eb70d1b
fgfl <frederick.gfl@gmail.com>
2020-05-28 11:14:45 -0700

    Add component and story

---
+++ src/components/LobbyCard/LobbyCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import ValueShower from '../ValueShower/ValueShower';
+import './LobbyCard.scss';
+
+export default function LobbyCard({
+  lobbyTitle,
+  lobbySize,
+  totalPlayers,
+  passwordRequired,
+  onClickJoin,
+}) {
+  const lobbyCardClass = classNames('lobby-card');
+
+  return (
+    <div className={lobbyCardClass}>
+      <h2>{lobbyTitle}</h2>
+      {passwordRequired && <p>P/W requred</p>}
+      <ValueShower value={`${totalPlayers}/${lobbySize}`}>Players</ValueShower>
+      <button onClick={onClickJoin}>Join</button>
+    </div>
+  );
+}
---
+++ src/components/LobbyCard/LobbyCard.test.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+
+import LobbyCard from '../components/LobbyCard/LobbyCard';
+
+afterEach(cleanup);
